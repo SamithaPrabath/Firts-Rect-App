b3a64e8d2403ab8fcc331a9cff4bdedac6239240
SamithaPrabath <samithaprabath513@gmail.com>
2022-11-19 19:08:07 +0530

    First-Commit

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+
+function App2(){
+    return(
+        <h2>Hello World</h2>
+    );
+}
+
+function App2Test(){
+    return(
+        <h2>Hello Everyone</h2>
+    );
+}
+
+export default App2;
+export {App2Test};
--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App2 from './App2';
+import {App2Test} from './App2';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App2Test/>
   </React.StrictMode>
+);
+
+const root1=ReactDOM.createRoot(document.getElementById("root1"));
+root1.render(
+  <React.StrictMode>
+    <App2/>
+    <App2Test/>
+  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
